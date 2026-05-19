31b0c4a77dcc1f2f70047e468e7bdd30413cb45b
jeremymz241-del <jeremymz241@gmail.com>
2026-05-19 11:09:28 +0200

    Delete config-db.php

--- config-db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=db_MAZOU;charset=UTF8','22507451','230352');
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
